37c66e571c3eaa2db56d23e4a372f5f7f61739f5
Alex <57219372+h3llfir3@users.noreply.github.com>
2020-10-15 17:45:21 +0200

    End of first class

---
+++ RomanCalc/RomanCalc/UserInterface.cs
@@ -0,0 +1,22 @@
+﻿
+namespace RomanCalc
+{
+    class UserInterface
+    {
+        public static void PrintMainMenu()
+        {
+            System.Console.WriteLine("          CALCULADORA ROMÁN");
+            System.Console.WriteLine("--------------------------------------");
+            System.Console.WriteLine("1) Sumar una serie de números.");
+            System.Console.WriteLine("2) Restar una serie de números.");
+            System.Console.WriteLine("3) Multiplicar una serie de números.");
+            System.Console.WriteLine("4) Dividir una serie de números.");
+            System.Console.WriteLine("5) Ecuaciones de primer grado.");
+            System.Console.WriteLine("6) Ecuaciones de segundo grado.");
+            System.Console.WriteLine("7) Raíces cuadradas.");
+            System.Console.WriteLine("8) Fibonacci.");
+            System.Console.WriteLine("9) Sumatorios.");
+            System.Console.WriteLine("0) Salir.\n");
+        }
+    }
+}
